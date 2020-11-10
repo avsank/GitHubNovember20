818208510c0fdca92ffd476d4fed865ef1736c5a
Anna Shabalina <shabalina@gmail.com>
2020-11-10 12:20:27 -0500

    Added new class

---
+++ src/FrequencyOfWord.java
@@ -0,0 +1,12 @@
+public class FrequencyOfWord {
+    public static void main(String[] args) {
+        String sentence = "Cat dog cat and dogs";
+        String word = "dog";
+        int count = 0;
+        while(sentence.contains(word)){
+            sentence = sentence.replaceFirst(word, "");
+            count++;
+        }
+        System.out.println(count);
+    }
+}
