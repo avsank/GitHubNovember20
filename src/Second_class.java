4be19d45e6f62ad40f6c4e61d140b63e0325c23c
Anna Shabalina <shabalina@gmail.com>
2021-01-22 21:08:39 -0500

    Anna's 2nd commit.

--- src/Second_class.java
+++ src/Second_class.java
@@ -2,5 +2,8 @@ public class Second_class {
     public static void main(String[] args) {
 
         System.out.println("Trying to do commit for the 1st time");
+        System.out.println("How its possible abstract without abstract methods. This question is like BE or Not to BE");
     }
+
+
 }
